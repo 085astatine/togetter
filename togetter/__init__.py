7943b0478f11dea2bda33187d4ca9a07c2b5ab51
085astatine <astatine085@gmail.com>
2015-06-08 19:22:12 +0900

    指定したUserが作成したPage一覧が取得可能になった
    TogetterUserPage
        指定したUserが作成したPageの一覧を対象とする
        Page遷移に対応している
        各PageをTogetterPageInfoとして返すことができる
    TogetterPageInfo
        タイトルとURL,PageIDを返す
        TogetterPageを呼び出すことができる
    getAllPagefromUser
        指定したUserが作成したPageを
        TogetterPageInfoとして返すIteratorを呼び出す関数

--- togetter/__init__.py
+++ togetter/__init__.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-
 from .togetter_data import TogetterData, fromXML
 from .togetter_page import TogetterPage, toXML
+from .togetter_user_page import TogetterUserPage, getAllPagefromUser
---
+++ togetter/togetter_user_page.py
@@ -0,0 +1,122 @@
+# -*- coding: utf-8 -*-
+
+import logging
+import re
+import time
+import requests
+from .webpage import WebPage
+from .togetter_page import TogetterPage
+
+class TogetterUserPage(WebPage):
+    def __init__(self, user_id, page= 1, session= None, logger= None):
+        # logger設定
+        if logger is None:
+            logger = logging.getLoger(__name__)
+        # 値設定
+        self._user_id = user_id
+        self._page_number = page
+        # 接続
+        url = r'http://togetter.com/id/{0}'.format(user_id)
+        params = {'page': page} if page != 1 else {}
+        if session is None:
+            session = requests.session()
+        self._session = session
+        WebPage.__init__(self, url,
+                         params= params,
+                         session= self._session,
+                         logger= logger)
+        # logger出力
+        self._logger.info('{0}'.format(self.__class__.__name__))
+        self._logger.info('  user id: {0}'.format(self._user_id))
+        self._logger.info('  page   : {0}'.format(self._page_number))
+        self._logger.info('  URL    : {0}'.format(self.url))
+    
+    @property
+    def user_id(self):
+        return self._user_id
+    
+    @property
+    def page_number(self):
+        return self._page_number
+    
+    def getPageList(self):
+        xpath = r'//ul[@class="simple_list"]/li[@class]'
+        return [TogetterPageInfo(data) for data in self.html.xpath(xpath)]
+    
+    def nextPage(self):
+        xpath = r'head/link[@rel="next"]'
+        exists = (len(self.html.xpath(xpath)) == 1)
+        if exists:
+            return TogetterUserPage(self.user_id,
+                                    page= self.page_number + 1,
+                                    session= self._session,
+                                    logger= self._logger)
+        else:
+            return None
+    
+    def prevPage(self):
+        xpath = r'head/link[@rel="prev"]'
+        exists = (len(self.html.xpath(xpath)) == 1)
+        if exists:
+            return TogetterUserPage(self.user_id,
+                                    page= self.page_number - 1,
+                                    session= self._session,
+                                    logger= self._logger)
+        else:
+            return None
+
+class TogetterPageInfo(object):
+    def __init__(self, element):
+        self._element = element
+    
+    @property
+    def element(self):
+        return self._element
+    
+    @property
+    def url(self):
+        xpath = r'./div[@class="inner"]/a[@href]'
+        data = self.element.xpath(xpath)
+        if len(data) == 1:
+            return data[0].get('href')
+        else:
+            return None
+    
+    @property
+    def title(self):
+        xpath = r'./div[@class="inner"]/a[@href]/h3[@title]'
+        data = self.element.xpath(xpath)
+        if len(data) == 1:
+            return data[0].text
+        else:
+            return None
+    
+    @property
+    def id(self):
+        url = self.url
+        if not url is None:
+            regex = re.match(r'http://togetter.com/li/(?P<id>[0-9]+)', url)
+            if not regex is None:
+                return int(regex.group('id'))
+            else:
+                return None
+        else:
+            return None
+    
+    def open(self, page= 1, session= None, logger= None):
+        page_id = self.id
+        if not page_id is None:
+            TogetterPage(page_id,
+                         page= page,
+                         session= session,
+                         logger= logger)
+        else:
+            return None
+
+def getAllPagefromUser(user_id, session= None, logger= None):
+    user_page = TogetterUserPage(user_id, session= session, logger= logger)
+    while not user_page is None:
+        for page_data in user_page.getPageList():
+            yield page_data
+        user_page = user_page.nextPage()
+        time.sleep(0.2)
