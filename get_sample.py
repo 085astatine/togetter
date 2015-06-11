5a4ad2cb989ae129c86ae253446b414d728972e6
085astatine <astatine085@gmail.com>
2015-06-11 13:23:44 +0900

    Sampleのimportを変更
    get_sample.py
        from togetter import ~~ -> import togetter

--- get_sample.py
+++ get_sample.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import logging
-from togetter import TogetterData, TogetterPage, fromXML
+import togetter
 
 if __name__=='__main__':
     print('TogetterSample')
@@ -14,13 +14,13 @@ if __name__=='__main__':
     logger.addHandler(handler)
     # get tweet from togetter
     togetter_id = XXXXXXX
-    page = TogetterPage(togetter_id, logger= logger)
+    page = togetter.TogetterPage(togetter_id, logger= logger)
     page.loadTweets()
     # save as XML
     xml_file = 'togetter_{0}.xml'.format(togetter_id)
     page.saveAsXML(xml_file)
     # load from XML
-    togetter_data = fromXML(xml_file)
+    togetter_data = togetter.fromXML(xml_file)
     print(togetter_data.title)
     print(togetter_data.url)
     for tweet in togetter_data.tweet_list:
