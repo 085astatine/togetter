de0662f8bc1cf1ef43a18c41bbd51943525f6fc5
085astatine <astatine085@gmail.com>
2015-06-14 19:42:10 +0900

    __all__を指定した
    __init__
        __all__にTogetterData等を設定した

--- togetter/__init__.py
+++ togetter/__init__.py
@@ -1,4 +1,14 @@
 # -*- coding: utf-8 -*-
+
+__all__ = [
+    'TogetterData',
+    'TogetterPage',
+    'TogetterUserPage',
+    'fromXML',
+    'toXML',
+    'getAllPagefromUser',
+    ]
+
 from .togetter_data import TogetterData, fromXML
 from .togetter_page import TogetterPage, toXML
 from .togetter_user_page import TogetterUserPage, getAllPagefromUser
