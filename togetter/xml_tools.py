7367d9e6be2974743e6275f07f13f82f0d2288c7
085astatine <astatine085@gmail.com>
2015-06-24 17:54:14 +0900

    整形されたXMLを出力する箇所が簡略化された
    togetter/xml_tools
        saveXML
            整形されたXMLを出力する際に,xml.dom.minidomを介さずとも良くなった.

--- togetter/xml_tools.py
+++ togetter/xml_tools.py
@@ -1,25 +1,14 @@
 # -*- coding: utf-8 -*-
 
 import pathlib
-import xml.dom.minidom
 import lxml.etree
 
 def saveXML(element_tree, xml_path, pretty_print= True):
     if not isinstance(xml_path, pathlib.Path):
         xml_path = pathlib.Path(xml_path)
     with xml_path.open(mode= 'w', encoding= 'utf-8', newline= '') as file:
-        # インデント整形された出力
-        if pretty_print:
-            temp_string = lxml.etree.tostring(
-                        element_tree,
-                        encoding= 'utf-8').decode('utf-8')
-            reparsed = xml.dom.minidom.parseString(temp_string)
-            file.write(reparsed.toprettyxml(
-                        encoding= 'utf-8',
-                        indent='    ').decode('utf-8'))
-        # 単行での出力
-        else:
-            file.write(lxml.etree.tostring(
-                        element_tree,
-                        encoding= 'utf-8',
-                        xml_declaration= True).decode('utf-8'))
+        file.write(lxml.etree.tostring(
+                    element_tree,
+                    encoding= 'utf-8',
+                    pretty_print= pretty_print,
+                    xml_declaration= True).decode('utf-8'))
