84e68d8d7f88708a0e6d3ae5bf17679e84ab5373
085astatine <astatine085@gmail.com>
2016-06-26 02:05:01 +0900

    WebPage annotationを追加
    WebPage
        関数の引数と返り値にannotationを記載した.
        docstringを変更した.

--- togetter/webpage.py
+++ togetter/webpage.py
@@ -2,21 +2,33 @@
 
 import logging
 import pathlib
+from typing import Union
 import lxml.html
 import requests
 
 class WebPage:
-    def __init__(self, url, session= None, params= None, logger= None):
-        """constructor
-        url(str): WebページのURL
-        session(requests.sessions.Session): URLを開くためのsession
-                                            (default None)
-        params(dict): URLに渡されるparameter (default None)
-        logger(logging.Logger): logger (default None)"""
+    def __init__(
+                self,
+                url: str,
+                session: requests.sessions.Session = None,
+                params: dict = None,
+                logger: logging.Logger = None) -> None:
+        """Initialize
+
+        Arguments:
+        url (str): URL to send.
+        session (requests.sessions.Session, optional):
+            A Requests session.
+            Defaults to None. Then new Session will be Createed.
+        params (dict, optional):
+            dictonary of URL parameters to append to the URL.
+            Defaults to None.
+        logger (logging.Logger, optional):
+            Logger
+            Defauults to None, then new Logger will be Created"""
         # logger設定
         if logger is None:
             logger = logging.getLogger(__name__)
-            logger.addHandler(logging.NullHandler())
         self._logger = logger
         # 値設定
         if session is None:
@@ -24,26 +36,29 @@ class WebPage:
         self._response = session.get(url, params= params)
         self._html = lxml.html.fromstring(self.response.content)
     
-    def page_title(self):
-        """header部に書かれているtitleを返す"""
-        xpath = r'head//title'
-        return self.html.xpath(xpath)[0].text
-    
     @property
-    def url(self):
+    def url(self) -> str:
         return self._response.url
     
     @property
-    def response(self):
+    def response(self) -> requests.Response:
         return self._response
     
     @property
-    def html(self):
+    def html(self) -> lxml.html.HtmlElement:
         return self._html
     
-    def save(self, filepath):
-        """page内容を保存する
-        filepath(str, pathlib.Path): 保存先となるfile"""
+    def page_title(self) -> str:
+        """Get page title from HTML header"""
+        xpath = r'head//title'
+        return self.html.xpath(xpath)[0].text
+    
+    def save(self, filepath: Union[str, pathlib.Path]) -> None:
+        """Save the contents of the pages in the file
+
+        Args:
+        filepath (str, pathlib.Path): Filepath to save the contents
+        """
         if isinstance(filepath, str):
             filepath = pathlib.Path(filepath)
         with filepath.open(mode= 'wb') as file:
